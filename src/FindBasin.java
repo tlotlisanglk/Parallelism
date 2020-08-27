5748f040f3f84dcc486ae8b60e37a90d798a0834
Tlotlisang Lekena <tlotlisanglk@gmail.com>
2020-08-27 21:05:15 +0200

    The program runs properly but isn't excuting as expected

--- src/FindBasin.java
+++ src/FindBasin.java
@@ -1,44 +1,46 @@
 import java.util.ArrayList;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 class FindBasin extends  RecursiveTask<ArrayList<String>> {
-    static final int SEQUENTIAL_THRESHOLD = 1200;
+    static final int SEQUENTIAL_THRESHOLD = 100000;
 
     public int lo;
     public int hi;
-    public float[][] basinarr;
+    public float[][] arr;
 
     FindBasin(float[][] a, int l, int h) {
-        lo=l; hi=h;
-        basinarr = a;
+        lo=l;
+        hi=h;
+        arr = a;
     }
 
     protected ArrayList<String> compute() {
         if(hi - lo <= SEQUENTIAL_THRESHOLD) {
-            int ans = 0;
-            ArrayList<String> list_of_indexes = new ArrayList<>();
+
+            ArrayList<String> list = new ArrayList<>();
             for(int i=lo; i < hi; ++i){
                 for(int j=lo; j < hi; ++j){
-                    if (i != 0 &&j != 0 && j != basinarr.length-1 &&i != basinarr.length-1){
-                        if ((basinarr[i][j] +0.01)<= basinarr[i - 1][j - 1] && (basinarr[i][j] +0.01) <= basinarr[i - 1][j ] && (basinarr[i][j] +0.01) <= basinarr[i-1][j + 1] && (basinarr[i][j] +0.01) <= basinarr[i][j - 1] && (basinarr[i][j] +0.01) <= basinarr[i][j+1] && (basinarr[i][j] +0.01) <= basinarr[i + 1][j - 1] && (basinarr[i][j] +0.01) <= basinarr[i+1][j] && (basinarr[i][j] +0.01) <= basinarr[i + 1][j + 1]) {
-                            String indexes = i + " " + j;
-                            list_of_indexes.add(indexes);
+                    if (i != 0 &&j != 0 && j != arr.length-1 &&i != arr.length-1){
+                        if ((arr[i][j] +0.01)<= arr[i - 1][j - 1] && (arr[i][j] +0.01) <= arr[i - 1][j ] && (arr[i][j] +0.01) <= arr[i-1][j + 1] && (arr[i][j] +0.01) <= arr[i][j - 1] && (arr[i][j] +0.01) <= arr[i][j+1] && (arr[i][j] +0.01) <= arr[i + 1][j - 1] && (arr[i][j] +0.01) <= arr[i+1][j] && (arr[i][j] +0.01) <= arr[i + 1][j + 1]) {
+                            String indexes = i + " and " + j;
+                            System.out.println(indexes);
+                            list.add(indexes);
                         }
                     }
                 }
             }
-            return list_of_indexes;
+            return list;
         }
         else {
             int mid = lo + (hi - lo) / 2;
-            FindBasin left = new FindBasin(basinarr,lo,mid);
-            FindBasin right = new FindBasin(basinarr,mid,hi);
+            FindBasin left = new FindBasin(arr,lo,mid);
+            FindBasin right = new FindBasin(arr,mid,hi);
             left.fork();
             ArrayList <String> rightAns = right.compute();
+            System.out.println(rightAns.size());
             ArrayList <String> leftAns = left.join();
-            leftAns.addAll(rightAns);
-            return leftAns;
+            rightAns.addAll(leftAns);
+            return rightAns;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -17,36 +17,41 @@ public class Main
     }
     static final ForkJoinPool fjPool = new ForkJoinPool();
     public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(new BufferedReader(new FileReader("data/med_in.txt")));
-        while(sc.hasNextDouble()) {
-            int columns = sc.nextInt();
-            int rows =sc.nextInt();
-            float [][] myArray = new float[rows][columns];
-            for (int i=0; i<rows; i++) {
-                for (int j=0; j<columns; j++) {
-                    myArray[i][j] = sc.nextFloat();
-                }
+
+        // pass the path to the file as a parameter
+        tick();
+        File file = new File("data/large_in.txt");
+        Scanner sc = new Scanner(file);
+        String[] size = sc.nextLine().split(" ");
+        String[] data =  sc.nextLine().split(" ");
+        int rows = Integer.parseInt(size[0]);
+        int cols = Integer.parseInt(size[1]);
+        float[][] array = new float[rows][cols];
+        File filee = new File("data/med_check.txt");
+        FileWriter fr = new FileWriter(filee, true);
+
+        for(int i=0; i< rows ; i++) {
+            for(int j=0 ; j<cols;j++) {
+                array[i][j] = Float.parseFloat(data[i+j]);
             }
-            tick();
-            ArrayList<String> f = basinTerrain(myArray);
-            System.out.println("Basins No. :"+f.size());
-            float time = tock();
-
-            File file = new File("data/med_out_text.txt");
-            FileWriter fr = new FileWriter(file, true);
-            for(String line: f){
-                String l = line+"\n";
-                fr.write(l);
-            }
-            fr.write("Run took "+ time +" seconds");
-            fr.close();
-            System.out.println(f);
-
-
         }
+        fr.close();
 
+        ArrayList<String> f = findBasin(array);
+        System.out.println("Basins No. :"+data.length);
+        float time = tock();
+
+        File fileex = new File("data/med_out_text.txt");
+        FileWriter frr = new FileWriter(fileex, true);
+        for(String line: f){
+            String l = line+"\n";
+            frr.write(l);
+        }
+        frr.write("Run took "+ time +" seconds");
+        frr.close();
+        System.out.println(f);
     }
-    static  ArrayList<String>  basinTerrain(float[][] array)
+    static  ArrayList<String>  findBasin(float[][] array)
     {
         return ForkJoinPool.commonPool().invoke(new FindBasin(array,0,array.length));
 
